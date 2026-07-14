a21dea84f0d2c2f12e02d7734114cfa47d009cec
Rene <rene.winchenbach@gmail.com>
2026-07-14 12:27:57 +0100

    fix covariance export

--- src/sphWarpCore/sph.py
+++ src/sphWarpCore/sph.py
@@ -1,10 +1,9 @@
 from .diffusion import *
-from .renorm import computeSPHCovariance_warpBackend, computeRenormalizationMatrices
+from .renorm import computeRenormalizationMatrices
 from .operations import sphOperation_warp
 
 __all__ = [
     "sphOperation_warp",
-    "computeSPHCovariance_warpBackend",
     "computeRenormalizationMatrices",
     # diffusion
     "computeDiffusionWarp",
